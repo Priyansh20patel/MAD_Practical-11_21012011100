48a52a3875a34ada5e3d21d7ea252510aa7df37f
Priyansh <priyashpatel21@gnu.ac.in>
2023-11-01 22:04:57 +0530

    P-11

--- app/src/main/java/com/priyanshpatel/mad_practical11_21012011100/PersonAdapter.kt
+++ app/src/main/java/com/priyanshpatel/mad_practical11_21012011100/PersonAdapter.kt
@@ -5,42 +5,57 @@ import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ArrayAdapter
-import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
-import androidx.recyclerview.widget.RecyclerView.ViewHolder
-
-class PersonAdapter( val context: Context,val personArray:ArrayList<Person>):RecyclerView.Adapter<PersonAdapter.PersonViewHolder>()
-
-{
-    inner class PersonViewHolder(val view:View):ViewHolder(view)
-
-
-
+import java.io.Serializable
+class PersonAdapter(private val context: Context, private val array: ArrayList<Person>):
+    RecyclerView.Adapter<PersonAdapter.PersonViewHolder>(){
+    lateinit var databaseHelper: DatabaseHelper
+    init {
+        // Initialize the databaseHelper here
+        databaseHelper = DatabaseHelper(context)
+    }
+    inner class PersonViewHolder(val itemView: View): RecyclerView.ViewHolder(itemView)
+    {
+        val nameTxt : TextView = itemView.findViewById(R.id.name)
+        val emailTxt : TextView = itemView.findViewById(R.id.emailid)
+        val phoneTxt : TextView = itemView.findViewById(R.id.mobile)
+        val addressTxt : TextView = itemView.findViewById(R.id.address)
+        val mapBtn : ImageView = itemView.findViewById(R.id.btnLocation)
+        val deleteBtn : ImageView = itemView.findViewById(R.id.btnDelete)
+    }
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PersonViewHolder {
-        val view=LayoutInflater.from(context).inflate(R.layout.person_item,parent,false)
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.person_item, parent, false)
         return PersonViewHolder(view)
     }
-
     override fun getItemCount(): Int {
-        return personArray.size
+        return array.size
     }
-
     override fun onBindViewHolder(holder: PersonViewHolder, position: Int) {
-       val view = holder.view
-        val contact=personArray.get(position)
-
-        view.findViewById<TextView>(R.id.person_name).text = contact?.name
-        view.findViewById<TextView>(R.id.personnumber).text = contact?.phoneNo
-        view.findViewById<TextView>(R.id.personemail).text = contact?.emailId
-        view.findViewById<TextView>(R.id.personaddress).text = contact?.address
-        val button1: Button = view.findViewById(R.id.locationBtn)
-        button1.setOnClickListener {
-            // Start the MapsActivity when button1 is clicked
-            val intent = Intent(context, MapsActivity::class.java)
-            context.startActivity(intent)
+        val person = array[position]
+        holder.nameTxt.text = person.name
+        holder.emailTxt.text = person.emailId
+        holder.phoneTxt.text = person.phoneNo
+        holder.addressTxt.text = person.address
+        val obj = person as Serializable
+        databaseHelper.insertPerson(person)
+        holder.mapBtn.setOnClickListener {
+            Intent(this@PersonAdapter.context, MapsActivity::class.java).apply {
+                putExtra("Object",obj)
+                this@PersonAdapter.context.startActivity(this)
+            }
         }
-
+        holder.deleteBtn.setOnClickListener {
+            var count = databaseHelper.deletePerson(person.id)
+            if(count > 0)
+            {
+                Toast.makeText(this.context, "${person.name}'s details deleted successfully!",
+                    Toast.LENGTH_SHORT).show()
+                array.removeAt(position)
+                notifyDataSetChanged()
+            }
+        }
     }
 }
